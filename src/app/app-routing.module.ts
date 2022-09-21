5761b07fe014fc5f24a67c47d7544c7183012a60
Anjalee1999 <anjaleehansikakapukotuwa@gmail.com>
2022-09-21 14:21:40 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MachineComponent } from '../app/machine/machine.component';
+import { CompanyComponent } from '../app/company/company.component';
+import { InvoiceComponent } from '../app/invoice/invoice.component';
+import { ReportComponent } from '../app/report/report.component';
+import { AccountComponent } from '../app/account/account.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'machine',
+    component: MachineComponent,
+  },
+  {
+    path: 'company',
+    component: CompanyComponent,
+  },
+  {
+    path: 'invoice',
+    component: InvoiceComponent,
+  },
+  {
+    path: 'report',
+    component: ReportComponent,
+  },
+  {
+    path: 'account',
+    component: AccountComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MachineComponent } from './machine/machine.component';
+import { CompanyComponent } from './company/company.component';
+import { InvoiceComponent } from './invoice/invoice.component';
+import { ReportComponent } from './report/report.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MachineComponent,
+    CompanyComponent,
+    InvoiceComponent,
+    ReportComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
